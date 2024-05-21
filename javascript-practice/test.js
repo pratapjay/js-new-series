0210ce69eee9f4c34072572f08f44ea22f50aee6
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-21 10:44:07 +0000

    container config done

--- javascript-practice/test.js
+++ javascript-practice/test.js
@@ -2,9 +2,10 @@ const accountId=25555
 let accountEmail="Pratapjay684@gmail.com"
 var accountPassword="12354"
 accountCity="Jaipur"
-// console.log([accountId,accountEmail,accountPassword,accountCity])
+let accountState;
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 accountEmail="new@gmail.com"
 accountPassword="5687"
 accountCity="Delhi"
-console.log([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
