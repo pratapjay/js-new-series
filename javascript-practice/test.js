00488b2902d87095290496ddaff1bef282ba8209
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-21 10:32:28 +0000

    container config done

---
+++ javascript-practice/test.js
@@ -0,0 +1,10 @@
+const accountId=25555
+let accountEmail="Pratapjay684@gmail.com"
+var accountPassword="12354"
+accountCity="Jaipur"
+// console.log([accountId,accountEmail,accountPassword,accountCity])
+
+accountEmail="new@gmail.com"
+accountPassword="5687"
+accountCity="Delhi"
+console.log([accountId,accountEmail,accountPassword,accountCity])
