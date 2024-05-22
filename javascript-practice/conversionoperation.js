7ab925481c03b8816004f55c6b8a46f32c717715
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-22 07:38:11 +0000

    container config done

---
+++ javascript-practice/conversionoperation.js
@@ -0,0 +1,21 @@
+// let score="33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let ValueInNumber=Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// let score=null;
+
+// console.log(typeof score);
+
+// let ValueInNumber=Number(score);
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+let isLoggedIn=0
+let booleaIsnLoggedIn=Boolean(isLoggedIn);
+console.log(booleaIsnLoggedIn)
+
