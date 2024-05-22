dc8a3dae299a5b9bbdca1a74136251492d24584d
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-22 06:33:47 +0000

    container config done

---
+++ javascript-practice/datatype.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3);
+
+console.log("Jay Pratap");
+
+console.log(typeof "Jay Pratap");
+let age=18;
+console.log(typeof age);
+
