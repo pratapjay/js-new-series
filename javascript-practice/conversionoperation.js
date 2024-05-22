8f18bee33e4020d0d309935f96d210516d1c78ee
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-22 08:06:24 +0000

    done

--- javascript-practice/conversionoperation.js
+++ javascript-practice/conversionoperation.js
@@ -15,7 +15,36 @@
 // console.log(typeof ValueInNumber);
 // console.log(ValueInNumber);
 
-let isLoggedIn=0
-let booleaIsnLoggedIn=Boolean(isLoggedIn);
-console.log(booleaIsnLoggedIn)
+// let isLoggedIn=0
+// let booleaIsnLoggedIn=Boolean(isLoggedIn);
+// console.log(booleaIsnLoggedIn)
 
+
+
+// ****************************Operation***************************
+
+let Value = 3;
+let negValue = -Value;
+console.log(negValue);   //negative value
+
+let str1="Jay"
+let str2=" Pratap"
+let str3=str1 + str2;
+console.log(str3)
+
+
+
+// prefix and postfix
+// let a=3;
+// const b=a++;
+// console.log(a,b);
+
+
+let a=3
+const b=++a
+console.log(a,b)
+
+
+let x=3n
+const y=x++
+console.log(x,y);
