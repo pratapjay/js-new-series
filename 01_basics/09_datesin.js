5d710caf215b3de70b40362dd0437cad8cd76b65
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-27 16:50:14 +0000

    done

---
+++ 01_basics/09_datesin.js
@@ -0,0 +1,14 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+
+let newCreatedDate = new Date(2024,5 ,29, 6, 7 )
+// console.log(newCreatedDate.toLocaleString());
