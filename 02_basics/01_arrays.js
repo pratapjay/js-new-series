17c30dcb395f53ce90385b29a1613079a078f91e
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-28 07:30:56 +0000

    done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -9,7 +9,7 @@ console.log(myArray.length)
 // Array Method
 
 // push operation
-const myArray = [0,1,2,3,4]
+// const myArray = [0,1,2,3,4]
 myArray.push(9)
 console.log(myArray)
 
@@ -23,35 +23,35 @@ myArr.unshift(9);
 console.log(myArr);
 
 //Shift operation
-const myArr = [0,2,3,5,6,9]
+// const myArr = [0,2,3,5,6,9]
 myArr.shift()
 console.log(myArr)
 
 //includes operation
-const myArr = [1,2,3,5,6,9]
+// const myArr = [1,2,3,5,6,9]
 console.log(myArr.includes(4))
 
 // index operation
-const myArr = [1,4,5,6,8]
+// const myArr = [1,4,5,6,8]
 console.log(myArr.indexOf(8))
 
 // join 
-const myArr = [1,4,5,6,8]
+// const myArr = [1,4,5,6,8]
 const newArr = myArr.join()
 console.log(newArr)
 console.log(typeof newArr)
 
 // slice, splice
 //slice
-const myArr = [1,2,3,4,5,6]
-console.log("A",myArr)
-const myn1 = myArr.slice(1,3)
-console.log("B", myArr)
+const myArr1 = [1,2,3,4,5,6]
+console.log("A",myArr1)
+const myn1 = myArr1.slice(1,3)
+console.log("B", myArr1)
 console.log(myn1)
 
 //splice
-const myArr = [1,2,3,4,5,6]
-console.log("A",myArr)
-const myn1 = myArr.splice(1,3)
-console.log("B", myArr)
-console.log(myn1)
+const myArr2 = [1,2,3,4,5,6]
+console.log("A",myArr2)
+const myn2 = myArr2.splice(1,3)
+console.log("B", myArr2)
+console.log(myn2)
