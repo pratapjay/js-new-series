34fa168b52b82c6385c899211acc1e0b9a3d6209
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-29 16:55:28 +0000

    done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -10,22 +10,31 @@ const JSUser = {
     email: "pratapjay684@gmail.com",
     isLoggedIn: false,
 }
-console.log(JSUser.name)         //This is not a write way to print any variable
-console.log(JSUser["name"]);     //This is Valid
+// console.log(JSUser.name)         //This is not a write way to print any variable
+// console.log(JSUser["name"]);     //This is Valid
 
 //console.log(JSUser.Full Name) //(Full Name)This is not valid in object (We cannot access this value using dot notation)
-console.log(JSUser["Full Name"]) // This is Valid
+// console.log(JSUser["Full Name"]) // This is Valid
 
-console.log(JSUser[mySym]);
+// console.log(JSUser[mySym]);
 
 // we can overwrite a values
-JSUser.name = "Prashant"
-console.log(JSUser)
+// JSUser.name = "Prashant"
+// console.log(JSUser)
  
 // We can freeze the value also
-Object.freeze(JSUser)
+// Object.freeze(JSUser)
 
 // after freeze operation We can not overwrite values
-JSUser.name = "Prashant"
-console.log(JSUser)
+// JSUser.name = "Prashant"
+// console.log(JSUser)
 
+JSUser.greeting = function(){
+    console.log("Hello World");
+}
+console.log(JSUser.greeting);
+
+JSUser.greeting2 = function(){
+    console.log(`Hello World, ${this.email} `);
+}
+console.log(JSUser.greeting2());
