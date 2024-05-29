96520df5958d141e4843ee8238b8b065f53559e7
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-05-29 16:40:50 +0000

    done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,31 @@
+// object literals  
+const mySym = Symbol("Prashant") // This is symbol
+
+const JSUser = {
+    name: "JayPratap",
+    "Full Name": "JaypratapSingh",
+    [mySym]: "Prashant", // this is a way of define symbol in object 
+    age: 18,
+    location: "Jaipur",
+    email: "pratapjay684@gmail.com",
+    isLoggedIn: false,
+}
+console.log(JSUser.name)         //This is not a write way to print any variable
+console.log(JSUser["name"]);     //This is Valid
+
+//console.log(JSUser.Full Name) //(Full Name)This is not valid in object (We cannot access this value using dot notation)
+console.log(JSUser["Full Name"]) // This is Valid
+
+console.log(JSUser[mySym]);
+
+// we can overwrite a values
+JSUser.name = "Prashant"
+console.log(JSUser)
+ 
+// We can freeze the value also
+Object.freeze(JSUser)
+
+// after freeze operation We can not overwrite values
+JSUser.name = "Prashant"
+console.log(JSUser)
+
