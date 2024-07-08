ae1b6884c610e3493a49a8858d91cd6f76e3d0e7
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-07-08 10:14:39 +0000

    done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,3 +6,35 @@ if(true){
 }
 console.log(a);
 // console.log(b);
+
+function one(){
+    const username = "Jay Pratap"
+    function Two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    Two()
+}
+one()
+
+
+if(true){
+    const username="Jay Pratap";
+    if(username==="Jay Pratap"){
+        const website="Youtube"
+        console.log(username+" "+website);
+    }
+}
+
+
+//++++++++++++++++++++++++++++++++//
+console.log(addOne(5))
+function addOne(num){
+    return num+1;
+}
+
+
+const addTwo=function(num){
+    return num+2
+}
+addTwo(56)
