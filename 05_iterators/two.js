4025b11f4a238ffc488200faf949710f5a06a598
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-07-13 21:08:58 +0000

    done

---
+++ 05_iterators/two.js
@@ -0,0 +1,13 @@
+// while loop
+let i=0
+while (i<=10) {
+    console.log(`2 * ${i} = ${2*i} `);
+    i++;
+}
+
+// do while loop
+let score = 100
+do{
+    console.log(`score is ${score}`);
+    score++
+} while(score<=10);
