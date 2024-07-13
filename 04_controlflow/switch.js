5a723f5f4cfefb35be42d290e847095ab6dd1ec4
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-07-13 19:11:06 +0000

    done

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,40 @@
+
+// // for number
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    default:
+        console.log("default case");
+        break;
+}
+
+// string
+const months = "march"
+switch (months) {
+    case "Jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("default case");
+        break;
+}
