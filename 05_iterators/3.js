c60610d20f562613d15aa87fda7f445dbbf64940
Jay Pratap <82452694+pratapjay@users.noreply.github.com>
2024-07-13 21:43:54 +0000

    done

---
+++ 05_iterators/3.js
@@ -0,0 +1,40 @@
+// for of
+const arr = [1,2,3,4,5,6]
+for (const num of arr) {
+    // console.log(num);
+}
+
+//Example
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    // console.log(greet);
+}
+
+// Maps
+// map is not take same value. in other words we can say map always store unique value
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United state of America")
+map.set('Fr',"France")
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+// }
+
+for (const [key, value] of map) {
+    //
+    console.log(key, ':-', value);
+}
+
+
+// object
+// this will not work here
+const myObject = {
+    "Name": "Jay Pratap",
+    "Class": 7,
+    "School": "new"
+}
+for (const key of myObject) {
+    console.log(key);
+}
